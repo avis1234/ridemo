af9e14b168194728c26a8463ea4e5bf765271735
Avi Segal <avisegal@gmail.com>
2011-09-16 17:47:44 +0200

    Fixed pre compiling of css for production issue

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ Ridemo::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
 
-  match "/avi" => "Welcome#index"
+  # match "/avi" => "Welcome#index"
   # root :to => "Welcome#index"
 
   # See how all your routes lay out with "rake routes"
